b28545e70924711f343cf87bad66304ecd5c94ab
Brandon238 <brandonoseko@gmail.com>
2023-03-21 09:40:07 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,34 +6,43 @@
  *
  * @n: print from this number
  *
- * Return: 0 (Success)
+ * Return: always 0
  */
 
 void print_to_98(int n)
 {
 	int i;
-	int j;
 
-	if (n <= 98)
+	if (n >= 0 && n <= 98)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
+			printf("%d", i);
 			if (i != 98)
-				printf("%d,", i);
-			else if (i == 98)
-				printf("%d\n", i);
+				printf(", ");
 		}
+		printf("\n");
 	}
-	else if (n >= 98)
+	else if (n < 0)
 	{
-		for (j = n ; j <= 98 ; j--)
+		for (i = n ; i <= 98 ; i++)
 		{
-			if (j != 98)
-				printf("%d,", j);
-			else if (j == 98)
-				printf("%d\n", j);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
+		printf("\n");
 	}
+	else if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+		printf("%d", n);
 }
-
-
