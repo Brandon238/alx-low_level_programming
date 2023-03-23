88e39f9ae0b884125fae79ddf18279530f76e210
Brandon238 <brandonoseko@gmail.com>
2023-03-23 05:16:30 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - function to be performed
+ *
+ * @c: parameter to be checked
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
