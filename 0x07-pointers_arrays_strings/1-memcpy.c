f98e99d56efea0fb9ea3ee09c17e92f562cce810
Brandon238 <brandonoseko@gmail.com>
2023-04-03 04:06:22 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - function to be tested
+ * @dest: memory area where copied
+ * @src: memory area where stored
+ * @n: number of bytes
+ *
+ * Return: copied memory with n byte changed
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i ; i++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
